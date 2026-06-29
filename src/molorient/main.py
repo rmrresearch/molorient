5162eedbd857db27777d81c572cb572b14322503
jtl13 <jtlewis313@gmail.com>
2026-06-29 15:33:01 -0500

    Added main.py

---
+++ src/molorient/main.py
@@ -0,0 +1,28 @@
+from molorient.classes.atom import Atom
+from molorient.utils.orient_system import orient_system
+from molorient.utils.cli import read_xyz, get_precision
+import argparse
+import os
+
+
+def main():
+    """
+    Runs read_xyz(), get_precision(), and orient_system() to standardize geometry.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="Path to file")
+    args = parser.parse_args()
+    atoms, folder, base, ext = read_xyz(args.file)
+    get_precision()
+    std_atoms = orient_system(atoms)
+    std_xyz_filepath = os.path.join(folder, f"{base}_standardized{ext}")
+
+    with open(std_xyz_filepath, 'w') as f:
+        f.write(f"{len(std_atoms)}\n")
+        f.write("Standardized geometry by molorient\n")
+        for atom in std_atoms:
+            f.write(f"{atom.element} {atom.x} {atom.y} {atom.z}\n")
+
+
+if __name__ == '__main__':
+    main()
